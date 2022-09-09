ee2527fa0f8e6228f6fe7cc8e0a4301f2b66d6d4
abhayhp <dadhwal.abhay.abhay@gmail.com>
2022-09-09 15:56:09 +0530

    Add project files.

---
+++ 8th_sep_ConsoleApp1/HW_loops.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8th_sep_ConsoleApp1
+{
+    internal class HW_loops
+    {
+        static void Main(string[] args)
+        {
+            // 1) to print numbers 1-50.
+            //int i = 1;
+            //while (i <= 50)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            // 2) to print even numbers till 20.
+            //int i = 1;
+            //while(i <= 20)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            // 3) to print odd numbers in descending order between 50 and 100. 
+            //int i = 100;
+            //while( i >= 50)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i--;
+            //}
+
+            // 4) to print arithmatic table of a number
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //int i = 1;
+            //while(i <= 10)
+            //{
+            //    Console.WriteLine(num * i);
+            //    i++;
+
+            // 5) to print numbers between 1-100 that are divisible by 5.
+            //int i = 1;
+            //while( i <= 100)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+            
+
+        }
+    }
+}
---
+++ 8th_sep_ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8th_sep_ConsoleApp1
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+           
+        //    for (int i = 1; i <= 50; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+
+            //}
+
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+
+            //}
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for ( int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(num * i);
+            //}
+
+            //for (int i = 2; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+
+            //}
+
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int result = 1;
+
+            //for (int i = num; i > 0; i--)
+            //{
+            //    result *= i;
+
+
+            //}
+            //Console.WriteLine(result);
+
+
+            //int n_o_d = 9;
+            //int n1 = 0, n2 = 1, n3;
+            //Console.WriteLine($"{n1} ");
+            //Console.WriteLine($"{n2} ");
+
+
+            //for (int i = 2; i < n_o_d; i++)
+            //{
+            //    n3 = n1 + n2;
+            //    Console.WriteLine(n3);
+            //    n1 = n2;
+            //    n2 = n3;
+            //}
+
+        //}
+    }
+}
